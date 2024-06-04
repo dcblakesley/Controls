a60b6e511775d3a61c74d97ec39282888931e243
David Blakesley <dblakesley@webstaurantstore.com>
2024-06-03 20:42:58 -0700

    _

---
+++ FormTesting/FormTesting.Client/BasicEditors/FormData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Linq.Expressions;
+
+namespace FormTesting.Client.BasicEditors;
+
+public class FormData
+{
+    public FormData(FieldIdentifier fieldIdentifier, List<Attribute> attributes)
+    {
+        FieldIdentifier = fieldIdentifier;
+        Attributes = attributes;
+        Name = AttributesHelper.GetLabelText(Attributes, FieldIdentifier);
+        Id = AttributesHelper.GetId(Id, Name);
+        Description = AttributesHelper.GetDescription(Attributes);
+    }
+
+    public FieldIdentifier FieldIdentifier { get; set; }
+    public EditContext EditContext { get; set; }
+    public List<Attribute> Attributes { get; set; }
+    public string? Description { get; set; }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public bool IsRequired { get; set; }
+}
